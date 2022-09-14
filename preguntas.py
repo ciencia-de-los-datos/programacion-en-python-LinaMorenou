7f765d75756272aec7e57bcb60768b7ce14af1d8
LinaMorenou <111617441+LinaMorenou@users.noreply.github.com>
2022-09-14 16:05:25 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -16,19 +16,22 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
+    
     Rta/
     214
+    
     """
-    infoCol = open("data.csv", "r").readlines() #Se hace lectura de la data
-    infoCol = [z.replace("\n", "") for z in infoCol] #Retiramos el retorno de carro
-    infoCol = [z.split("\t") for z in infoCol] #Separamos
-    data2 = [z[1] for z in infoCol[0:]] #Capturamos la columna 2
+    infoCol = open("data.csv", "r").readlines() 
+    infoCol = [z.replace("\n", "") for z in infoCol] 
+    infoCol = [z.split("\t") for z in infoCol] 
+    data2 = [z[1] for z in infoCol[0:]] 
 
     suma = 0
     for i in data2:
         suma = suma + int(i)
 
     return suma
+
 
 def pregunta_02():
     """
